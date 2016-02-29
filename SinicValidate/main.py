cdf005acb51c36c8879a1121f837905bb059d912
Brightcells <brightcells@gmail.com>
2016-02-29 17:28:33 +0800

    update phone number sections' regex

--- SinicValidate/main.py
+++ SinicValidate/main.py
@@ -30,10 +30,12 @@ import re
 class SinicValidate(object):
     def __init__(self):
         # Refer: http://www.oschina.net/code/snippet_238351_48624
-        self.ChinaMobile = r'^134[0-8]\d{7}$|^(?:13[5-9]|147|15[0-27-9]|178|18[2-478])\d{8}$'  # 移动方面最新答复
-        self.ChinaUnion = r'^(?:13[0-2]|145|15[56]|176|18[56])\d{8}$'  # 向联通微博确认并未回复
-        self.ChinaTelcom = r'^(?:133|153|177|18[019])\d{8}$'  # 1349号段 电信方面没给出答复，视作不存在
-        self.OtherTelphone = r'^170([059])\d{7}$'  # 其他运营商
+        # Refer: http://baike.baidu.com/view/12118274.htm
+        # Refer: http://news.imobile.com.cn/articles/2016/0204/164468.shtml
+        self.ChinaMobile = r'^134\d{8}$|^(?:13[5-9]|147|15[0-27-9]|178|18[2-478])\d{8}$'
+        self.ChinaUnion = r'^(?:13[0-2]|145|1[578][56])\d{8}$'
+        self.ChinaTelcom = r'^(?:133|153|177|18[019])\d{8}$'
+        self.OtherTelphone = r'^170(?:[0-257-9])\d{7}|^171\d{8}$'  # 虚拟运营商
 
         self.email_regex = r'^.+@([^.@][^@]+)$'
 
@@ -74,7 +76,6 @@ class SinicValidate(object):
         return re.match(regex or self.email_regex, message)
 
 
-# For backwards compatibility
 _global_instance = SinicValidate()
 phone = _global_instance.phone
 email = _global_instance.email
